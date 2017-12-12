f351e3f570a3a6df4a26621ed9743c0919cfa921
vikas <vikaskumarsingh9838@gmail.com>
2017-12-12 23:21:53 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import DataChart from './components/data_chart'
+
+
+
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <DataChart/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/data_chart.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react'
+import Request from 'superagent'
+import { Bar, Pie, Line, Doughnut, Scatter, HorizontalBar, Radar, Polar, Bubble } from 'react-chartjs-2'
+
+
+var femaleCount = []
+var countryList = []
+var defaultStyle = {
+    minWidth: '25%', height: '400px', float: 'left',
+    border: '1px solid #d2d2d2', background: '#fff', borderRadius: '2px'
+}
+
+var defaults = {
+    maintainAspectRatio: false,
+    title: {
+        display: true,
+        text: 'Popolation of Women in respective countries',
+        fontSize: 25
+    },
+    legend: {
+        display: true,
+        position: 'top'
+    }
+}
+
+class DataChart extends Component {
+    constructor() {
+        super();
+        this.state = {
+            chartData: {},
+            loading: true
+
+        }
+    }
+    componentWillMount() {
+        this.callApi().then(response => {
+            this.setState({
+                chartData: response.chartData,
+                loading: false
+            })
+        })
+    }
+
+    callApi() {
+        var url = "http://api.population.io/1.0/population/1980/aged/18/"
+        return Request.get(url).then((response) => {
+            let data = [];
+            let labels = response.body.slice(0, 5).reduce((countries, c) => {
+                data.push(c.total)
+                return countries.concat(c.country)
+            }, []);
+
+            return {
+                chartData: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Population',
+                            data: data,
+                            backgroundColor: 'rgba(255,99,132,0.2)',
+                            borderColor: 'rgba(255,99,132,1)',
+                            borderWidth: 1,
+                            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                            hoverBorderColor: 'rgba(255,99,132,1)'
+                            
+                        }
+                    ]
+                }
+
+            }
+        })
+    }
+
+    render() {
+        if (this.state.loading) {
+            console.log('This happens 4th - when waiting for data.');
+            return <h2>Loading...</h2>;
+          }
+
+        return (
+            <div>
+                <section>
+                    <h3>
+                        World Population Overview Dashboard
+                        </h3>
+                </section>
+                <div className="chart" style={defaultStyle}>
+                    <Bar
+                        width={100}
+                        height={50}
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+                <div className="chart" style={defaultStyle}>
+                    <Pie
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+                <div className="chart" style={defaultStyle}>
+                    <Line
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+                <div className="chart" style={defaultStyle}>
+                    <Doughnut
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+                <div className="chart" style={defaultStyle}>
+                    <HorizontalBar
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+                <div className="chart" style={defaultStyle}>
+                    <Radar
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+                <div className="chart" style={defaultStyle}>
+                    <Polar
+                        data={this.state.chartData}
+                        options={defaults}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+export default DataChart;
